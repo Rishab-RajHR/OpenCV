f785e95d3bc76f2de3651d708dec0a189f058d79
Rishab Raj <rr0948555@gmail.com>
2026-02-07 14:08:10 +0530

    Contour

--- Contour & Shape Detection/contour_fun.py
+++ Contour & Shape Detection/contour_fun.py
@@ -5,4 +5,10 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 _, thresh = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)
 
 #FIND CONTOURS
-contours, heirarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+contours, heirarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+cv2.drawContours(img, contours, -1, (0,255,0), 3)
+
+cv2.imshow("Contours", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
