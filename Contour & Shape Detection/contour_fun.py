a5ffd795a8026f691fe2f05f3a4cb485ac6020c1
Rishab Raj <rr0948555@gmail.com>
2026-02-07 14:26:21 +0530

    Ravel

--- Contour & Shape Detection/contour_fun.py
+++ Contour & Shape Detection/contour_fun.py
@@ -9,6 +9,39 @@ contours, heirarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_S
 
 cv2.drawContours(img, contours, -1, (0,255,0), 3)
 
+for contour in contours:
+    approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, 2, True))
+
+    corners = len(approx)
+
+    if corners == 3:
+        shape_name = "Triangle"
+
+    elif corners == 4:
+        shape_name = "Rectangle"
+
+    elif corners == 5:
+        shape_name = "Pentagon"
+
+    elif corners > 5:
+        shape_name = "Circle"
+
+    else:
+        shape_name = "unknown"
+
+        cv2.drawContours(img, [approx], 0, (0,255,0), 2)
+        x = approx.ravel()[0]
+        """
+        [
+        [[100,200]],
+        [[150,250]],
+        [[120,270]],
+        ]
+        [100,200,150,250,120,270]
+        """
+        y = approx.ravel()[1] - 10
+        cv2.putText(img, shape_name, (x,y), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 0, 0), 2 )
+
 cv2.imshow("Contours", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
